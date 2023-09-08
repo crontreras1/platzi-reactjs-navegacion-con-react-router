bcac60937749da5a3ed40a36ea2efd6efb1a67e0
cristian contreras <cr.ontreras@gmail.com>
2023-09-08 14:08:12 -0500

    class 6. navegate components. the most basic form

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { HashRouter, Routes, Route} from 'react-router-dom'
+import { Menu } from './Menu'
+import { HomePage } from './HomePage'
+import { BlogPage } from './BlogPage'
+import { ProfilePage } from './ProfilePage'
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={ <HomePage />} />
+
+          <Route path='/blog' element={ <BlogPage />} />
+
+          <Route path='/profile' element={ <ProfilePage />} />
+          
+          // * Simboliza todas las rutas que no se han definido aún y podemos mostrar un error o algo por el estilo
+          <Route path='/*' element={ <h1>Página No Encontrada 😰</h1> } />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
